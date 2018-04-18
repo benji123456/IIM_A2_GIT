3e496a0cfc3c02aa74b78cf097341e5d3503a68c
Benjamin <benjaminbaloul@hotmail.com>
2018-04-18 14:47:09 +0300

    Modif login

--- validation.php
+++ validation.php
@@ -1,14 +1,37 @@
-<?php
+<?php session_start();
 require('config/config.php');
 require('model/functions.fn.php');
-session_start();
 
-if(	isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) && 
-	!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])) {
+/*===============================
+	Register
+===============================*/
 
-	// TODO
 
-}else{ 
-	$_SESSION['message'] = 'Erreur : Formulaire incomplet';
-	header('Location: register.php');
+if (isset($_POST['email']) && isset($_POST['password']) && isset($_POST['username'])) {
+    if (!empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['username'])) {
+
+        $username = htmlspecialchars($_POST['username']);
+        $email = htmlspecialchars($_POST['email']);
+        $password = htmlspecialchars($_POST['password']);
+
+        if(isUsernameAvailable($db, $username)) {
+            if(isEmailAvailable($db, $email)) {
+                userRegistration($db, $username, $email, $password);
+                header('Location: dashboard.php');
+            } else {
+                $error = "Email indisponible";
+            }
+        } else {
+            $error = "Username indisponible";
+        }
+
+
+
+    } else {
+        $error = 'Champs requis !';
+    }
 }
+
+include 'view/_header.php';
+include 'view/register.php';
+include 'view/_footer.php';
