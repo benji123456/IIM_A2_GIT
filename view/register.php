373c4e289031f7ac2bb994241b098f67019db4e2
Benjamin <benjaminbaloul@hotmail.com>
2018-04-18 14:50:35 +0300

    modif register

--- view/register.php
+++ view/register.php
@@ -23,7 +23,7 @@
 					</div>
 					<div class="form-input">
 						<span class="password">
-							<input type="password" name="password" placeholder="Password">
+							<input type="password" name="password" placeholder="password">
 						</span>
 					</div>
 
